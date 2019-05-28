073d6e978e31d2fe6912d07528be1c013adf2461
jonminton <nate.minton@gmail.com>
2019-05-28 18:31:22 +0100

    investigate age specific mortality

---
+++ scripts/country_definitions.R
@@ -0,0 +1,20 @@
+# Define high income countries used in White 2002
+
+high_income_countries <- 
+  c(
+    "AUS", "AUT", "BEL", "CAN", "DNK", "FIN",
+    "FRATNP", "DEUTW", "GRC", "IRL", "ITA", "JPN",
+    "NLD", "NZL_NP", "NOR", "PRT", "ESP", "SWE",
+    "CHE", "GBR_NP", "USA"
+  )
+
+# Define all distinct countries in HMD
+
+all_distinct_countries <- c(
+  "AUS", "AUT", "BEL", "BGR", "BLR", "CAN", "CHL", "HRV", "CHE",
+  "CZE", "DEUTE", "DEUTW", "DNK", "ESP", "EST", "FIN", "FRATNP", "GRC", "HUN",
+  "IRL", "ISL", "ISR", "ITA", "JPN", "KOR", "LTU", "LUX", "LVA", "NLD", "NOR",
+  "NZL_NP",
+  "POL", "PRT", "RUS", "SVK", "SN", "SWE", "TWN", "UKR", "GBR_NP", "USA"
+)
+
